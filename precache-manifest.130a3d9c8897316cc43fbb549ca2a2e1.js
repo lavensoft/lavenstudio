7007d86856e47e252678f3862f8e5ca210a7cfa5
Lavensoft <46833976+Lavensoft@users.noreply.github.com>
2020-09-14 00:52:12 +0700

    Updates

---
+++ precache-manifest.130a3d9c8897316cc43fbb549ca2a2e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c622fb60a53bb68c4bb6adf71c8c90c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cccfdd08fcf2e0d837f4",
+    "url": "/static/css/main.e20f7d28.chunk.css"
+  },
+  {
+    "revision": "a693ee4931cb3a2e3e42",
+    "url": "/static/js/2.b2a3a518.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b2a3a518.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cccfdd08fcf2e0d837f4",
+    "url": "/static/js/main.41db1d27.chunk.js"
+  },
+  {
+    "revision": "4a34379290c15a52d178",
+    "url": "/static/js/runtime-main.a9e1fb01.js"
+  }
+]);
